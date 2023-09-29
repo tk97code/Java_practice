2075810e4ad358f8f5ee127de5d6b06a0570d64d
allanMartin <62497108+tk97code@users.noreply.github.com>
2023-09-29 14:02:26 +0700

    Create DIY_ex08.java

---
+++ Chapter3/DIY_ex08.java
@@ -0,0 +1,10 @@
+// 08
+public class DIY_ex08 {
+	public static void main(String[] args) {
+		double avg = 0;
+		for (int i = 1; i < 101; i++) {
+			avg += i;
+		}
+		System.out.println("sum of 1-100: "+ avg/100);
+	}
+}
