e36288db15c2fff0f0708e325827e036a95eab8b
allanMartin <62497108+tk97code@users.noreply.github.com>
2023-09-29 16:08:18 +0700

    Create DIY_ex14.java

---
+++ Chapter3/DIY_ex14.java
@@ -0,0 +1,17 @@
+// 14
+public class DIY_ex {
+
+	public static void main(String[] args) {
+		int space_ = 7;
+		for (int i = 0; i < 4; i++) {
+			for (int k = 0; k < space_ - i; k++) {
+				System.out.print("   ");
+			}
+			for (int j = 0; j < i*2 + 1; j++) {
+				System.out.print("*  ");
+			}
+			System.out.println();
+		}
+	}
+
+}
