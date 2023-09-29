1a292ff6d66ca7f77a1095cb24e646e701ccd76e
allanMartin <62497108+tk97code@users.noreply.github.com>
2023-09-29 13:58:26 +0700

    Create DIY_ex07.java

---
+++ Chapter3/DIY_ex07.java
@@ -0,0 +1,10 @@
+// 07
+public class DIY_ex07 {
+	public static void main(String[] args) {
+		int sum = 0;
+		for (int i = 1; i < 101; i+= 2) {
+			sum += i;
+		}
+		System.out.println("sum of 1-100: "+ sum);
+	}
+}
