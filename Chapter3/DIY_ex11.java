4938c2ba15666428f44e8cef52bacefda0ebcbba
allanMartin <62497108+tk97code@users.noreply.github.com>
2023-09-29 14:20:03 +0700

    Create DIY_ex11.java

---
+++ Chapter3/DIY_ex11.java
@@ -0,0 +1,10 @@
+// 11
+public class DIY_ex11 {
+	public static void main(String[] args) {
+		for (int i = 1; i < 101; i++) {
+			System.out.print(i + " ");
+			if (i % 2 == 0) System.out.println("EVEN \n");
+			else System.out.println("UNEVEN \n");
+		}
+	}
+}
