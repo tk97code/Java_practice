d0ab9e6346fc57650b03bf477f4ce6a4ffbeee72
allanMartin <62497108+tk97code@users.noreply.github.com>
2023-09-30 09:55:45 +0700

    Create DIY_ex16.java

---
+++ Chapter3/DIY_ex16.java
@@ -0,0 +1,13 @@
+package Chapter3;
+
+public class DIY_ex16 {
+
+	public static void main(String[] args) {
+		// 0 3 8 15 24 35 48 -> Rule: n(n+2)
+		for (int n = 0; n <= 100; n++) {
+			System.out.println(n*(n+2));
+		}
+
+	}
+
+}
