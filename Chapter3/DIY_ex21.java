a6f63105e7ddc39f726a11475d29fce361321f60
allanMartin <62497108+tk97code@users.noreply.github.com>
2023-10-01 18:07:48 +0700

    Create DIY_ex21.java

---
+++ Chapter3/DIY_ex21.java
@@ -0,0 +1,10 @@
+public class DIY_ex21 {
+
+	public static void main(String[] args) {
+		for (int i = 0; i <= 0xFFFF; i++) {
+			System.out.println(i + " -> " + Integer.toHexString(i));
+		}
+		
+	}
+
+}
