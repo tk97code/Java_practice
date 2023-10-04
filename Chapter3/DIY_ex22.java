748b566b8af9ed9a19a83ccdfdfe13bdd95da5e2
allanMartin <62497108+tk97code@users.noreply.github.com>
2023-10-04 16:34:04 +0700

    Update DIY_ex22.java

--- Chapter3/DIY_ex22.java
+++ Chapter3/DIY_ex22.java
@@ -3,7 +3,7 @@ package Chapter3;
 public class DIY_ex22 {
 
 	public static void main(String[] args) {
-		for (int i = 0; i <= 777; i++) {
+		for (int i = 0; i <= 511; i++) {
 			System.out.println(i + " -> " + Integer.toOctalString(i));
         }
 	}
