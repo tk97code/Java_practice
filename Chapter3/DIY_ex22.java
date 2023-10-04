ed78ed6c098c071ce08801d0eeeaf894299bda2c
allanMartin <62497108+tk97code@users.noreply.github.com>
2023-10-04 12:07:24 +0700

    Create DIY_ex22.java

---
+++ Chapter3/DIY_ex22.java
@@ -0,0 +1,11 @@
+package Chapter3;
+
+public class DIY_ex22 {
+
+	public static void main(String[] args) {
+		for (int i = 0; i <= 777; i++) {
+			System.out.println(i + " -> " + Integer.toOctalString(i));
+        }
+	}
+
+}
