488b1ee019bc7a71e527ed4dd0e340f92debe996
allanMartin <62497108+tk97code@users.noreply.github.com>
2023-10-04 22:01:34 +0700

    Update DIY_ex01.java

--- Chapter4/DIY_ex01.java
+++ Chapter4/DIY_ex01.java
@@ -1,1 +1,25 @@
+package Chapter4;
+
+import java.util.*;
+
+public class DIY_ex01 {
+
+	public static void smallest(int[] arr) {
+		Arrays.sort(arr);
+		System.out.println("The smallest number is " + arr[0]);
+	}
+	
+	public static void main(String[] args) {
+		int[] arr = new int[3];
+		Scanner sc = new Scanner(System.in);
+		
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print("Input number " + (i+1) + " : ");
+			arr[i] = sc.nextInt();
+		}		
+		smallest(arr);
+		
+	}
+
+}
 
