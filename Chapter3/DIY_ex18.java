c12f0b20e848f83c9050b4cb1103ed317e85816e
allanMartin <62497108+tk97code@users.noreply.github.com>
2023-10-04 19:10:44 +0700

    Update DIY_ex18.java

--- Chapter3/DIY_ex18.java
+++ Chapter3/DIY_ex18.java
@@ -1,1 +1,15 @@
-// Khong tim ra quy luat, cuu t voi :<<<
+package Chapter3;
+
+public class DIY_ex18 {
+
+	public static void main(String[] args) {
+		// 1 7 16 37 79 173 -> Rule:
+		double s = 0;
+		for (int i = 1; i <= 10; i++) {
+			s = 1+6*(i-1)+1.5*(i-1)*(i-2)+1.5*(i-1)*(i-2)*(i-3)+(double)11/60*(i-1)*(i-2)*(i-3)*(i-4)*(i-5);
+			System.out.println(s);
+		}
+
+	}
+
+}
