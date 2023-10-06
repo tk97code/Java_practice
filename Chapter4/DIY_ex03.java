ed904d5d85861a97f6d685a56a4e17cfe6de3289
allanMartin <62497108+tk97code@users.noreply.github.com>
2023-10-06 15:50:29 +0700

    Create DIY_ex03.java

---
+++ Chapter4/DIY_ex03.java
@@ -0,0 +1,27 @@
+package Chapter4;
+
+import java.util.Scanner;
+
+public class DIY_ex03 {
+	public static int CountWord(String str) {
+		int count = 0;
+		boolean isWord = true;
+		String[] str_arr = str.split(" ");
+		
+		// Empty string
+		if (str == null || str.isEmpty())
+			return 0;
+		
+		count = str_arr.length;
+		
+		return count;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Input String: ");
+		String str = sc.nextLine();
+		
+		System.out.println("Number of words in String: " + CountWord(str));
+	}
+}
