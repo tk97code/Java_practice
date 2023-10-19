d7c1048f8dad75bf3085c0ae466d7e09bc0112d3
allanMartin <62497108+tk97code@users.noreply.github.com>
2023-10-19 21:57:40 +0700

    Create DIY_ex18.java

---
+++ Chapter4/DIY_ex18.java
@@ -0,0 +1,21 @@
+package Chapter4;
+
+import java.util.*;
+
+public class DIY_ex18 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str1, str2;
+		System.out.println("Input begin string: ");
+		str1 = sc.nextLine();
+		System.out.println("Input string to concat: ");
+		str2 = sc.nextLine();
+	
+		
+		
+		System.out.println("Concat 2 string: " + str1.concat(str2));
+		
+	}
+
+}
